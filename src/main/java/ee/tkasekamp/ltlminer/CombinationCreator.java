a70015c748d496aeb1655d0e134b873ae69d1f7e
Tõnis Kasekamp <toniskasekamp@gmail.com>
2015-03-17 19:06:40 +0200

    Combination creation improved
    Creates all possible combination with repetition. Added tests.

--- src/main/java/ee/tkasekamp/ltlminer/CombinationCreator.java
+++ src/main/java/ee/tkasekamp/ltlminer/CombinationCreator.java
@@ -8,40 +8,49 @@ public class CombinationCreator {
 
 	public ArrayList<String> createCombinations(String rule,
 			ArrayList<String> activities) {
-		// regex to find the places to work with
-		// \w(:)\s(activity)
+
 		ArrayList<String> rules = new ArrayList<>();
 
 		ArrayList<String[]> combos = new ArrayList<>();
 		int k = numberOfArguments(rule);
 		String[] input = activities.toArray(new String[activities.size()]);
-		System.out.println(input);
 		String[] branch = new String[k];
-		combine(input, k, 0, branch, 0, combos);
-		System.out.println(combos.size());
+		combine(input, k, branch, 0, combos);
 
 		// Actually combine stuff
 		for (String[] strings : combos) {
-			for (int i = 0; i < strings.length; i++) {
-				rules.add(rule.replaceAll("\\w(:)\\s(activity)", "$0" + " : \""
-						+ strings[i] + "\""));
-			}
+			rules.add(replaceText(rule, strings));
 		}
 
 		return rules;
 	}
 
-	// http://exceptional-code.blogspot.com/2012/09/generating-all-permutations.html
-	private void combine(String[] arr, int k, int startId, String[] branch,
-			int numElem, ArrayList<String[]> combos) {
+	/**
+	 * Generates all possible combinations from arr with lenght k. Allows
+	 * repetiton in the finshed arrays.
+	 * <code>http://exceptional-code.blogspot.com/2012/09/generating-all-permutations.html</code>
+	 * 
+	 * @param arr
+	 *            Where to take arguments from
+	 * @param k
+	 *            How deep to go
+	 * @param branch
+	 *            Current working array
+	 * @param numElem
+	 *            Current depth
+	 * @param combos
+	 *            Holds finished arrays
+	 */
+	public void combine(String[] arr, int k, String[] branch, int numElem,
+			ArrayList<String[]> combos) {
 		if (numElem == k) {
-			combos.add(branch);
+			combos.add(createCopy(branch));
 			return;
 		}
 
-		for (int i = startId; i < arr.length; ++i) {
+		for (int i = 0; i < arr.length; ++i) {
 			branch[numElem++] = arr[i];
-			combine(arr, k, ++startId, branch, numElem, combos);
+			combine(arr, k, branch, numElem, combos);
 			--numElem;
 		}
 	}
@@ -54,6 +63,48 @@ public class CombinationCreator {
 		while (matcher.find())
 			count++;
 		return count;
+	}
+
+	/**
+	 * Adds a default activity for every rule parameter. Finds places like
+	 * <code>A : activity</code> and replaces it with something like
+	 * <code>A : activity : "A"</code>.
+	 * 
+	 * @param rule
+	 * @param combination
+	 * @return
+	 */
+	private String replaceText(String rule, String[] combination) {
+		Pattern patt = Pattern.compile("\\w(:)\\s(activity)");
+		Matcher m = patt.matcher(rule);
+		StringBuffer sb = new StringBuffer(rule.length());
+
+		int counter = 0;
+		while (m.find()) {
+			String text = m.group(0);
+
+			text += " : \"" + combination[counter] + "\"";
+			m.appendReplacement(sb, Matcher.quoteReplacement(text));
+			counter++;
+		}
+		m.appendTail(sb);
+		return sb.toString();
+	}
+
+	/**
+	 * Creates a deep copy of a String array.
+	 * 
+	 * @param original
+	 *            String []
+	 * @return Copy of the original
+	 */
+	private String[] createCopy(String[] original) {
+		String[] copy = new String[original.length];
+		for (int i = 0; i < original.length; i++) {
+			copy[i] = original[i];
+		}
+
+		return copy;
 	}
 
 }
--- src/test/java/ee/tkasekamp/ltlminer/CombinationCreatorTest.java
+++ src/test/java/ee/tkasekamp/ltlminer/CombinationCreatorTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 public class CombinationCreatorTest {
 	CombinationCreator comboCreator;
+
 	@Before
 	public void setUp() throws Exception {
 		comboCreator = new CombinationCreator();
@@ -18,17 +19,46 @@ public class CombinationCreatorTest {
 	public void replaceOneTest() {
 		ArrayList<String> activities = new ArrayList<>();
 		activities.add("Skyrim");
-		String rule = "formula one( A: activity) = {} \n"
-				+ "<>( event == E );";
-		ArrayList<String> combos = comboCreator.createCombinations(rule, activities);
+		String rule = "formula one( A: activity) = {} \n" + "<>( event == A );";
+		ArrayList<String> combos = comboCreator.createCombinations(rule,
+				activities);
 		assertEquals(1, combos.size());
-		System.out.println(combos.toString());
 		assertTrue(combos.get(0).contains("A: activity : \"Skyrim\""));
 	}
-	
+
 	@Test
 	public void replaceTwoTest() {
-		fail("Not yet implemented");
+		ArrayList<String> activities = new ArrayList<>();
+		activities.add("Skyrim");
+		activities.add("Oblivion");
+		String rule = "formula two( A: activity, B: activity) = {} \n"
+				+ "( <>( event == A ) /\\ <>( event == B) );";
+		ArrayList<String> combos = comboCreator.createCombinations(rule,
+				activities);
+		assertEquals(4, combos.size());
+
+		assertTrue(combos.get(0).contains("A: activity : \"Skyrim\""));
+		assertTrue(combos.get(0).contains("B: activity : \"Skyrim\""));
+		assertTrue(combos.get(1).contains("A: activity : \"Skyrim\""));
+		assertTrue(combos.get(1).contains("B: activity : \"Oblivion\""));
+		assertTrue(combos.get(2).contains("A: activity : \"Oblivion\""));
+		assertTrue(combos.get(2).contains("B: activity : \"Skyrim\""));
 	}
 
+	@Test
+	public void whiteSpaceTest() {
+		// tests for all combinations of whitespace. the regex must handle that.
+	}
+
+	@Test
+	public void combinerTest() {
+		int k = 2;
+		String[] input = new String[] { "A", "B", "C", "D", "E" };
+		String[] branch = new String[k];
+		ArrayList<String[]> combos = new ArrayList<>();
+		comboCreator.combine(input, k, branch, 0, combos);
+
+		assertEquals("Number of combinations", 25, combos.size());
+		assertEquals(k, combos.get(0).length);
+	}
 }
